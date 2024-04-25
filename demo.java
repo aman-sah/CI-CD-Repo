c28f7f3ea181a272846f5c65ff71fa5246f783b8
Aman <125241580+aman-sah@users.noreply.github.com>
2024-04-25 23:35:35 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 #import java.util.*;
 public class java{
 public static void main(String args[]){
-       System.out.println("Hello World!");
+       System.out.println("Hello World!"); // hello world
     }
 }
